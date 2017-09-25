19209c850a39d6805f7700d0d9e55eb399a85699
mohammedAli1800 <32209704+mohammedAli1800@users.noreply.github.com>
2017-09-25 01:06:12 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,36 @@
+<html>
+<!-- the header of the assgn -->
+<h1> welcome to My First Web Page</h1>
+<body>
+	<form action="http://www.example.com/bookings/"
+	 method="post">
+	<head><title> login</title></head>
+	
+	<label for="username">UserName:</label><input type="text" iname="username"required="required" /></title><br />
+	
+	<label for="password">Password:</label><input type="password" name="password" required="required" /><br/><br/>
+	<button type="submit">login</button>
+	
+	<?php>
 
-get_header(); ?>
+		 $UserName= array["ali","mohamed"];
+		 $flen = count($UserName);
+		for($i=0;i<$flen;$i++){
+			echo $UserName[$i];
+			if(UserName[i]=="ali"){
+				echo $UserName;
+			}
+			else if(UserName[i]== "mohamed"){
+				txt"is in the list";
+			}
+			else{
+				document.write("is not in the list");
+			}
+		}
+		?>
+	
 
-<div id="main-content" class="main-content">
-
-<?php
-	if ( is_front_page() && twentyfourteen_has_featured_posts() ) {
-		// Include the featured content template.
-		get_template_part( 'featured-content' );
-	}
-?>
+	
+	</body>
+	</html>
+	
