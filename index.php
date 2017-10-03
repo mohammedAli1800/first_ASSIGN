8b703035ee613428f16e3c064eca20f0fd2f1fc7
mohammedAli1800 <32209704+mohammedAli1800@users.noreply.github.com>
2017-10-03 01:14:25 -0400

    add fil

--- index.php
+++ index.php
@@ -1,42 +1,45 @@
+<?php
+ session_start();
+ if(isset($_GET['logout'])){
+    if($_GET['logout']==true){
+        $_SESSION['logged'] = false;
+        session_destroy();
+        echo "logged out";
+ }
+}
+if(isset($_SESSION["logged"])){
+    if($_SESSION["logged"]==true){
+    header("Location: welcome.php");
+    die();
+    }
+}
+
+?>
+
 <html>
 <!-- the header of the assgn -->
 <h1> welcome to My First Web Page</h1>
-<?php
-if(isset($_GET["usr"])){
-        
+<!-- if it there -->
 
- $userData["mohammed"] = "1234";
- $userData["ali"] = "1122";
-  $userData["test"] = "0000";
-         foreach($userData as $usrName => $Password){
-                 if($usrName ==$_GET['usr']){
-                         if($Password==$_GET['pass']){
-
-                                 echo("<p><h1>credentials is correct</h1><p>");
-                                 die();
-                         }
-                        
-                 }
-         }
-echo("<p><h1>credentials is not correct!!!<p><h1>");
-	echo(" <a href= 'index.php'<p><hi> click here</h1><p> </a>");
-
-}
- ?>
-        <form>
+    <form action="loginHandler.php" method="post">
 	<p><head>login</head></p>
-	<b>
-	<label for="username">UserName:</label><input type="text" id="username"name="usr"required="required" /></br>
-	
+	<label for="username">UserName:</label><input type="text" id="username" name="usr" required="required"/></br>
 	<label for="password">Password:</label><input type="text" id="password" name="pass" required="required" /></br></br>
-	<button type="submit" onclick="ubmitInfo()"> login2</button>
-	
-	</b>
+	<button type="submit"> login</button>
 	</form>
-        <script>
-        function submitInfo() {
-            location.href='\index.php?usr='+ document.getElementById('username').value + '&pass='+document.getElementById('password').value;
-        
-        </script>
-	</html>
-        
+
+<?php
+    if(isset($_POST['attempt'])){
+        if(isset($_SESSION['failed'])){
+            echo " <a> you tried " . $_SESSION['failed'] . " time/s to login </a>";
+        }else{
+        echo "no storred attempts";
+        }
+    }
+?>
+<form method="POST" action=''>
+<input type="submit" name="attempt"  value="attempts">
+</form>
+</html>
+
+      
